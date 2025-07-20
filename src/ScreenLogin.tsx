1cbaedb68a2c4cc815a4d77ab799cebbcbb4f7f6
Joao Victor <134668287+jvjoaovictorguedes@users.noreply.github.com>
2025-07-20 17:08:20 -0300

    feat: Create screen login

---
+++ src/ScreenLogin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "./components/Login/Login";
+
+export default function ScreenLogin() {
+    return (
+        <div className="homeMe w-full h-screen flex items-center bg-cover justify-center">
+            <Login />
+        </div>
+    )
+}
